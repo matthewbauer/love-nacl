fbe15f1cb98ca06d319e21c4a3bc559e3eb8a5fb
Matthew Bauer <mjbauer95@gmail.com>
2014-04-23 15:34:00 -0500

    updated data for hawkthorne

--- data/inject.js
+++
@@ -1,9 +0,0 @@
-if (document.embeds[0] &&
-    document.embeds[0].name === 'plugin' &&
-    document.embeds[0].type === 'application/x-love-game') {
-  if (!document.body.getAttribute('application_x_love_game_injected')) {
-    document.body.setAttribute('application_x_love_game_injected', 'true');
-    chrome.runtime.sendMessage('mompnkcmpbopandjnddeecgeeojegohc',
-                               'injected');
-  }
-}
